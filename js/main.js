67d192d9f75636a347912b9ed958452b0de73ccd
Lenovo <drvirus15@gmail.com>
2026-01-10 12:38:08 +0200

    digi_massive_changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-$(document).ready(function () {
-  $(".testimonial__carousel").owlCarousel(
-    {
-      items: 1,
-      loop: true,
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 6000
-    }
-  );
-});
+/* carousel for section intro */
 $(document).ready(function () {
   $(".intro__carousel").owlCarousel(
     {
@@ -20,6 +10,18 @@ $(document).ready(function () {
       '<span class="material-symbols-sharp">chevron_left</span>',
       '<span class="material-symbols-sharp">chevron_right</span>'
     ],
+    }
+  );
+});
+/* carousel for quotes section */
+$(document).ready(function () {
+  $(".testimonial__carousel").owlCarousel(
+    {
+      items: 1,
+      loop: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 6000
     }
   );
 });
